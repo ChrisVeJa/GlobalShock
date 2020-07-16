d6fa1b89f6b6eca290799f300e2768d781dd3a9f
chrisveja1103 <chrisveja28@gmail.com>
2020-07-15 22:42:19 -0400

    last updates

--- data.jl
+++ data.jl
@@ -1,25 +1,21 @@
 ################################################################################
-#    GLOBAL SHOCKS: Data maker heterogeneous response in Small Open Economies
+# GLOBAL SHOCKS: Data maker
+# I will cut every dataset at 2018Q4
 # Written by: Christian Velasquez
-# Any comment, doubt or suggestion just send me an email to velasqcb@bc.edu
+# velasqcb@bc.edu
 ################################################################################
 using Random, DataFrames, XLSX, LinearAlgebra, Statistics,
 	StatsBase, Distributions, Plots, CSV, RCall;
-
-
+dir1 = "G:/My Drive/GlobalShocks/data";
 # ------------------------------------------------------------------------------
 # Quaterly labels
 # ------------------------------------------------------------------------------
-qlabel = Array{String,2}(undef, 164, 1)
-for i = 1:164
-    year = string(1980 + div(i - 1, 4))
+qlabel = Array{String,2}(undef, 84, 1)
+for i = 1:84
+    year = string(1998 + div(i - 1, 4))
     quaterly = "Q" * string(mod(i - 1, 4) + 1)
     qlabel[i] = year * quaterly
 end
-
-# ------------------------------------------------------------------------------
-# 	DATA
-# ------------------------------------------------------------------------------
 
 #= ------------------------------------------------------------------------
 	[1.1] GDP G20
@@ -27,31 +23,27 @@ end
 	We select volume index
  The quaterly data starts at `1998Q1`
  ------------------------------------------------------------------------ =#
-
-g20 =
-    DataFrame(CSV.File("./Data/DP_LIVE_14072020212217545.csv"));
+g20 = DataFrame(CSV.File("$dir1/DP_LIVE_14072020212217545.csv"));
 g20 = g20[g20.LOCATION.=="G-20", :];
-g20 = g20[:, [1, 6, 7]];
-
+g20 = g20[1:end-4, [1, 6, 7]];
+g20 = log.(g20[:,3])
 #= ------------------------------------------------------------------------
  	[1.2] BAA spread
 	link: https://fred.stlouisfed.org/series/BAAFFM
 	serie: Moody's Seasoned Baa Corporate Bond Minus Federal Funds Rate
 	rename to FRED and save in xlsx format
-
- The quaterly data starts at `1980Q1`
+ The quaterly data starts at `1998Q1`
  ------------------------------------------------------------------------ =#
-
-y = DataFrame(CSV.File("./Data/BAAFFM.csv"));
+y = DataFrame(CSV.File("$dir1/BAAFFM.csv"));
+y = y[217:end-18,:];
 Tmonth = size(y)[1];
 baaq = zeros(Int(floor(Tmonth / 3)));
 for i = 1:Tmonth
     if mod(i, 3) == 0
         j = div(i, 3)
-        baaq[j] = mean(y[(j-1)*3+1:3*j, 2])
+        baaq[j] = mean(y[(j-1)*3+1:3*j, 2]);
     end
 end
-
 
 #= ------------------------------------------------------------------------
  [1.3]Import Price Index:Industrialized Countries- Manufactured articles
@@ -59,28 +51,25 @@ end
  The monthly data starts at `1990M12`
  The quaterly starts at 1994Q1
  ------------------------------------------------------------------------ =#
-y = DataFrame(CSV.File("./Data/INDUSMANU.csv"));
-y = parse.(Float64,convert(Vector,y[38:end,2]));
+y = DataFrame(CSV.File("$dir1/INDUSMANU.csv"));
+y = y[86:end-18,:];
+y = parse.(Float64,convert(Vector,y[:,2]));
 Tmonth = size(y)[1];
 indusq = zeros(Int(floor(Tmonth / 3)));
 for i = 1:Tmonth
     if mod(i, 3) == 0
         j = div(i, 3)
-        indusq[j] = mean(y[(j-1)*3+1:3*j])
+        indusq[j] = mean(y[(j-1)*3+1:3*j])/100
     end
 end
+
 #= ------------------------------------------------------------------------
 	[1.4] Commodity Prices
 		Link: https://www.imf.org/en/Research/commodity-prices
 		Commodity Data Portal
-
- The quaterly data starts at `1992Q1`
  ------------------------------------------------------------------------ =#
-y = XLSX.readdata(
-    "./Data/ExternalData.xlsx",
-    "Values!A149:CJ490",
-);
-y = y[:, 1:2]
+y = XLSX.readdata("$dir1/ExternalData.xlsx","Values!A149:CJ490");
+y = y[73:end-18, 1:2];
 Tmonth = size(y)[1];
 commq = zeros(Int(floor(Tmonth / 3)));
 for i = 1:Tmonth
@@ -89,15 +78,17 @@ for i = 1:Tmonth
         commq[j] = mean(y[(j-1)*3+1:3*j, 2])
     end
 end
+commq = commq ./ indusq; # Real price of COMMODITIES
+commq = log.(commq);
 
 # ------------------------------------------------------------------------
 # Graphics for global variables
 # ------------------------------------------------------------------------
 
-Wvar = [log.([g20[1:84, 3] commq[25:108]]) baaq[73:156]];
+Wvar = [g20 commq baaq];
 wvar = Wvar[2:end, :] - Wvar[1:end-1, :];
 wvar = [100 * wvar[:, 1:2] 0.5 * Wvar[2:end, 3]];
-Wlabel = qlabel[73:156];
+Wlabel = qlabel[2:end];
 
 T = size(wvar)[1];
 p = plot(
@@ -107,7 +98,7 @@ p = plot(
     legendfontsize = 6,
     label = ["G20 output" "BAA spread"],
     legend = :bottomleft,
-    xticks = (1:12:T, Wlabel[2:12:T+1]),
+    xticks = (1:12:T, Wlabel[1:12:T]),
     w = [1 2.5],
     style = [:dash :dot],
     c = [:black :gray],
@@ -119,7 +110,7 @@ p = twinx();
 plot!(
     p,
     wvar[:, 2],
-    ylims = (-50, 40),
+    ylims = (-45, 40),
     legend = false,
     xticks = :none,
     c = :red,
@@ -127,7 +118,7 @@ plot!(
     alpha = 0.75,
     ygridstyle = :dash,
 );
-plot!(NaN .* (1:T), c = :red, label = "Commodity prices");
+plot!(NaN .* (1:T), c = :red, label = "Real commodity prices");
 savefig(".//Figures//intro1.svg");
 
 #= ------------------------------------------------------------------------
@@ -144,11 +135,13 @@ savefig(".//Figures//intro1.svg");
  ------------------------------------------------------------------------ =#
 
 y = XLSX.readdata(
-    "./Data/Commodity_Terms_of_Trade_Commodity_.xlsx",
+    "$dir1/Commodity_Terms_of_Trade_Commodity_.xlsx",
     "data!A2:AKF184",
 )
 y= [y[:, 1] y[:, 2:2:end]];
 y[1, 1] = "Country";
+y= [y[:,1] y[:,218:469]];
+
 list =
     ["Argentina" "Brazil" "Chile" "Colombia" "South Africa" "Peru" "Australia" "Canada" "New Zealand" "Norway"];
 index = [findfirst(x -> x == i, y[:, 1]) for i in list];
@@ -158,25 +151,21 @@ y = y[:, 2:end];
 # ------------------------------------------------------------------------
 # Quarterly commodities
 Tmonth = size(y)[2];
-dataCq = zeros(10, Int(floor(Tmonth / 3)));
+tot   = zeros(10, Int(floor(Tmonth / 3)));
 for i = 1:Tmonth
     if mod(i, 3) == 0
         j = div(i, 3)
-        dataCq[:, j] = mean(y[:, (j-1)*3+1:3*j], dims = 2)
+        tot[:, j] = mean(y[:, (j-1)*3+1:3*j], dims = 2)
     end
 end
-dataCq     = log.(dataCq);
-dataCqyty  = dataCq[:, 5:end] - dataCq[:, 1:end-4];
-dataCqyty2 =
-    (dataCqyty .- mean(dataCqyty, dims = 2)) ./ std(dataCqyty, dims = 2);
-
-dataCqyty  = convert(Array, dataCqyty'); # Matrix form
-dataCqyty2 = dataCqyty2';# Standarized variables
-
-T  = size(dataCqyty)[1];
+tot   = tot' ./ indusq;
+tot   = log.(tot);
+dtot  = tot[5:end,:] - tot[1:end-4,:];
+dtot2 = (dtot .- mean(dtot, dims = 1)) ./ std(dtot, dims = 1);
+T  = size(dtot)[1];
 p1 = plot(layout = grid(3, 1), size = (1000, 1000))
 plot!(
-    dataCqyty[:, 1:3],
+    dtot[:, 1:3],
     c = [:black :red :teal],
     markershape = [:none :circle :none],
     w = [1 1.5 1],
@@ -195,7 +184,7 @@ plot!(
     tickfontsize = 10,
 );
 plot!(
-    dataCqyty[:, 4:6],
+    dtot[:, 4:6],
     c = [:black :red :teal],
     markershape = [:none :circle :none],
     w = [1 1.5 1],
@@ -214,7 +203,7 @@ plot!(
     tickfontsize = 10,
 );
 plot!(
-    dataCqyty[:, 7:10],
+    dtot[:, 7:10],
     c = [:black :red :teal :purple],
     markershape = [:none :circle :none :none],
     w = [1 1.5 1 1],
@@ -222,7 +211,7 @@ plot!(
     markercolor = :red,
     markerstrokewidth = 0.1,
     style = [:solid :dot :solid :solid],
-    xticks = (1:24:T, qlabel[5:24:T]),
+    xticks = (1:24:T, qlabel[5:24:T+4]),
     grid = false,
     label = [list[7] list[8] list[9] list[10]],
     legendfontsize = 9,
@@ -235,10 +224,9 @@ plot!(
 plot(p1);
 savefig(".//Figures//comm1.svg");
 
-
 p2 = plot(layout = grid(3, 1), size = (1000, 1000))
 plot!(
-    dataCqyty2[:, 1:3],
+    dtot2[:, 1:3],
     c = [:black :red :teal],
     markershape = [:none :circle :none],
     w = [1 1.5 1],
@@ -257,7 +245,7 @@ plot!(
     tickfontsize = 10,
 );
 plot!(
-    dataCqyty2[:, 4:6],
+    dtot2[:, 4:6],
     c = [:black :red :teal],
     markershape = [:none :circle :none],
     w = [1 1.5 1],
@@ -276,7 +264,7 @@ plot!(
     tickfontsize = 10,
 );
 plot!(
-    dataCqyty2[:, 7:10],
+    dtot2[:, 7:10],
     c = [:black :red :teal :purple],
     markershape = [:none :circle :none :none],
     w = [1 1.5 1 1],
@@ -284,7 +272,7 @@ plot!(
     markercolor = :red,
     markerstrokewidth = 0.1,
     style = [:solid :dot :solid :solid],
-    xticks = (1:24:T, qlabel[5:24:T]),
+    xticks = (1:24:T, qlabel[5:24:T+4]),
     grid = false,
     label = [list[7] list[8] list[9] list[10]],
     legendfontsize = 9,
@@ -304,21 +292,22 @@ savefig(".//Figures//comm2.svg");
 		The indices starts at 1994-1
  ------------------------------------------------------------------------ =#
 y  = XLSX.readdata(
-     "./Data/broad.xlsx",
+     "$dir1/broad.xlsx",
      "Real!A4:BI322",
 );
+y = y[1:302,:];
 index = [findfirst(x -> x == i, y[1, :]) for i in list];
 y  = dropdims(y[:,index], dims=2);
-y  = y[3:end,:];
+y  = y[51:end,:];
 Tmonth = size(y)[1];
-reerq  = zeros(Int(floor(Tmonth / 3)),10);
+reer  = zeros(Int(floor(Tmonth / 3)),10);
 for i = 1:Tmonth
     if mod(i, 3) == 0
         j = div(i, 3)
-        reerq[j,:] = mean(y[(j-1)*3+1:3*j,:], dims = 1)
+        reer[j,:] = mean(y[(j-1)*3+1:3*j,:], dims = 1)
     end
 end
-
+reer = log.(reer);
 #= ------------------------------------------------------------------------
  	[-] NATIONAL ACCOUNTS
 	[]  https://data.imf.org/regular.aspx?key=61545852
@@ -366,7 +355,7 @@ end
 	Base : 2000
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ =#
 basey = 2000;
-y    = DataFrame(XLSX.readdata("./Data/GDP1.xlsx", "Quarterly!B8:DU31"));
+y    = DataFrame(XLSX.readdata("$dir1/GDP1.xlsx", "Quarterly!B8:DU31"));
 aux1 = convert(Vector{Float64},y[12,17:end]);
 	# -----------------------
 	# Removing the seasonality
@@ -388,47 +377,55 @@ ec1  = ec1 ./ seas;
 
 stq = (basey-1993)*4+1;
 ec1 = [YBase(ec1[:,1],stq) YBase(ec1[:,2],stq) YBase(ec1[:,3],stq) ec1[:,4]];
-
+ec1 = ec1[21:104,:];
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # Brazil
 # Start at 1996Q1
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-y  = DataFrame(XLSX.readdata("./Data/GDP3.xlsx", "Quarterly!B8:DP30"));
+y  = DataFrame(XLSX.readdata("$dir1/GDP3.xlsx", "Quarterly!B8:DP30"));
 y  = convert(Matrix,y[[14,15,17,19,20,23],25:end]);
 ec2= Dataextract(y);
 stq = (basey-1996)*4+1;
 ec2 = [YBase(ec2[:,1],stq) YBase(ec2[:,2],stq) YBase(ec2[:,3],stq) ec2[:,4]];
-
+ec2 = ec2[9:92,:];
 
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # Chile
 # Start at 1996Q1
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-y  = DataFrame(XLSX.readdata("./Data/GDP5.xlsx", "Quarterly!B8:DU30"));
+y  = DataFrame(XLSX.readdata("$dir1/GDP5.xlsx", "Quarterly!B8:DU30"));
 y  = convert(Matrix,y[[14,15,17,19,20,23],29:end]);
 ec3= Dataextract(y);
 stq= (basey-1996)*4+1;
 ec3= [YBase(ec3[:,1],stq) YBase(ec3[:,2],stq) YBase(ec3[:,3],stq) ec3[:,4]];
-
+ec3 = ec3[9:92,:];
 #= +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
  Colombia
  * It is already adjusted by
  seasonallity, but I need to chain
  the data to have a sample from 1994
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ =#
-y  = DataFrame(XLSX.readdata("./Data/ColGDP.xlsx", "data!A3:CW8"));
+y  = DataFrame(XLSX.readdata("$dir1/ColGDP.xlsx", "data!A3:CW8"));
 y  = convert(Matrix,y[2:end, 2:end]);
 y  = y';
 stq= (basey-1994)*4+1;
 ec4= [YBase(y[:,1],stq) YBase(y[:,2],stq) YBase(y[:,3],stq) y[:,4]];
-
-# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ec4= ec4[17:100,:];
+#= +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # Peru
-# ** It was adjusted with XArima13
-# Starts at 1990Q1
-# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-y  = DataFrame(XLSX.readdata("./Data/GDP9.xlsx", "Quarterly!B8:DJ19"));
+ ** It was adjusted with XArima13
+ Starts at 1990Q1
+ https://estadisticas.bcrp.gob.pe/estadisticas/series/trimestrales/pbi-gasto
+ Series:
+	PN02529AQ 	Demanda Interna - Consumo Privado
+	PN02533AQ  	Demanda Interna - Inversión Bruta Interna -
+				Inversión Bruta Fija - Privada
+	PN02536AQ	Exportaciones
+	PN02537AQ	Importaciones
+	PN02538AQ	PBI
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ =#
+y  = DataFrame(XLSX.readdata("$dir1/Trimestral.xlsx", "Quarterly!B8:DJ19"));
 y  = convert(Matrix,y[[3,4,6,8,9,12],5:end]);
 deflator = y[6,:]';
 y  = y ./ deflator;
@@ -450,68 +447,87 @@ y[1:5,:] = y[1:5,:] ./ (seas');
 ec5= Dataextract(y);
 stq= (basey-1990)*4+1;
 ec5= [YBase(ec5[:,1],stq) YBase(ec5[:,2],stq) YBase(ec5[:,3],stq) ec5[:,4]];
-
+ec5 = ec5[33:116,:]
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # South Africa
 # Starts at 1990Q1
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-y  = DataFrame(XLSX.readdata("./Data/GDP10.xlsx", "Quarterly!B8:DI32"));
+y  = DataFrame(XLSX.readdata("$dir1/GDP10.xlsx", "Quarterly!B8:DI32"));
 y  = convert(Matrix,y[[15,16,18,20,21,25],5:end]);
 ec6= Dataextract(y);
 stq= (basey-1990)*4+1;
 ec6= [YBase(ec6[:,1],stq) YBase(ec6[:,2],stq) YBase(ec6[:,3],stq) ec6[:,4]];
-
+ec6 = ec6[33:116,:]
 
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # Australia
 # Starts at 1990Q1
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-y  = DataFrame(XLSX.readdata("./Data/GDP2.xlsx", "Quarterly!B8:DU32"));
+y  = DataFrame(XLSX.readdata("$dir1/GDP2.xlsx", "Quarterly!B8:DU32"));
 y  = convert(Matrix,y[[15,16,18,20,21,25],5:end]);
 dc1= Dataextract(y);
 stq= (basey-1990)*4+1;
 dc1= [YBase(dc1[:,1],stq) YBase(dc1[:,2],stq) YBase(dc1[:,3],stq) dc1[:,4]];
-
+dc1 = dc1[33:116,:];
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # Canada
 # Starts at 1990Q1
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-y  = DataFrame(XLSX.readdata("./Data/GDP4.xlsx", "Quarterly!B8:DS29"));
+y  = DataFrame(XLSX.readdata("$dir1/GDP4.xlsx", "Quarterly!B8:DS29"));
 y  = convert(Matrix,y[[12,13,15, 17, 18, 22],5:end]);
 dc2= Dataextract(y);
 stq= (basey-1990)*4+1;
 dc2= [YBase(dc2[:,1],stq) YBase(dc2[:,2],stq) YBase(dc2[:,3],stq) dc2[:,4]];
-
+dc2 = dc2[33:116,:];
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # New Zealand
 # Starts at 1990Q1
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-y  = DataFrame(XLSX.readdata("./Data/GDP7.xlsx", "Quarterly!B8:DU32"));
+y  = DataFrame(XLSX.readdata("$dir1/GDP7.xlsx", "Quarterly!B8:DU32"));
 y  = convert(Matrix,y[[15,16, 18,20,21,25],5:end]);
 dc3= Dataextract(y);
 stq= (basey-1990)*4+1;
 dc3= [YBase(dc3[:,1],stq) YBase(dc3[:,2],stq) YBase(dc3[:,3],stq) dc3[:,4]];
-
+dc3 = dc3[33:116,:];
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # Norway
 # Starts at 1990Q1
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-y  = DataFrame(XLSX.readdata("./Data/GDP8.xlsx", "Quarterly!B8:DV30"));
+y  = DataFrame(XLSX.readdata("$dir1/GDP8.xlsx", "Quarterly!B8:DV30"));
 y  = convert(Matrix,y[[14,15,17,19,20,23],5:end]);
 dc4= Dataextract(y);
 stq= (basey-1990)*4+1;
 dc4= [YBase(dc4[:,1],stq) YBase(dc4[:,2],stq) YBase(dc4[:,3],stq) dc4[:,4]];
-
+dc4 = dc4[33:116,:];
 
 #= ------------------------------------------------------------------------
  	[-] INTEREST RATES
 https://www.bis.org/statistics/cbpol.htm?m=6%7C382%7C679
 https://estadisticas.bcrp.gob.pe/estadisticas/series/mensuales/tasas-de-interes
  -------------------------------------------------------------------------- =#
-irate = XLSX.readdata("./Data/cbpol_2007.xlsx", "Monthly!A3:K321");
-y2    = XLSX.readdata("./Data/intratePer.xlsx", "Mensuales!A2:B296");
+irate = XLSX.readdata("$dir1/cbpol_2007.xlsx", "Monthly!A3:K321");
+y2    = XLSX.readdata("$dir1/intratePer.xlsx", "Mensuales!A2:B296");
 yy = [irate[26:end,10] y2[2:end,2]];
 dplot = convert(Matrix{Float64},yy[94:end,:]);
-plot(dplot);
+plot(dplot)
 # Then the overnight interbak lending rate is a good proxy of mp
 irate[26:end,10] = y2[2:end,2];
+# Now, putting in the correct order
+index = [findfirst(x -> x == i, irate[1,:]) for i in list];
+irate = dropdims(irate[50:end-18,index], dims=2);
+
+#= ------------------------------------------------------------------------
+	CREATING THE DATASET
+The order of the variables are:
+g20 >> comm >> baa >> gdp >> cons >> inv >> trade >> reer >> irate
+-------------------------------------------------------------------------- =#
+[g20 tot[:,1] baaq ec1 irate[:,1]]
+[g20 tot[:,2] baaq ec2 irate[:,2]]
+[g20 tot[:,3] baaq ec3 irate[:,3]]
+[g20 tot[:,4] baaq ec4 irate[:,4]]
+[g20 tot[:,5] baaq ec5 irate[:,5]]
+[g20 tot[:,6] baaq ec6 irate[:,6]]
+[g20 tot[:,7] baaq dc1 irate[:,7]]
+[g20 tot[:,8] baaq dc2 irate[:,8]]
+[g20 tot[:,9] baaq dc3 irate[:,9]]
+[g20 tot[:,10] baaq dc4 irate[:,10]]
